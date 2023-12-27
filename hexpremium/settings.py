97ebe5666a1b88ad6a1b6612b4e13b8f1b48c414
21stPhenom <phenom.h.acks21@gmail.com>
2023-12-27 14:28:44 +0100

    setup project for deployment

--- hexpremium/settings.py
+++ hexpremium/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG') == 'True'
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'hexpremium-production.up.railway.app']
 
 
 # Application definition
