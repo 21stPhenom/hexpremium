779614dc09b6d6b95800c2f6f892a3c25dd0d3cf
21stPhenom <phenom.h.acks21@gmail.com>
2023-12-29 14:41:57 +0100

    wrote logic for creating groups after migrating

--- accounts/groups.py
+++ accounts/groups.py
@@ -1,40 +1,54 @@
-from django.contrib.auth.models import Group
-from django.db.utils import OperationalError
-from accounts.models import Profile
+# Signal for creating groups after migrations
+def create_groups(sender, **kwargs):
+    from django.contrib.auth.models import Group
+    from accounts.permissions import get_permissions
+    permissions = get_permissions()
 
-try:
-    from accounts.permissions import FREE_PERMISSION, BASIC_PERMISSION, PRO_PERMISSION, PREMIUM_PERMISSION
-    FREE_GROUP, free_created = Group.objects.get_or_create(
-        name='Free Users',
-        permissions=[FREE_PERMISSION]
-    )
-    BASIC_GROUP, basic_created = Group.objects.get_or_create(
-        name='Basic Users',
-        permissions=[FREE_PERMISSION, BASIC_PERMISSION]
-    )
-    PRO_GROUP, pro_created = Group.objects.get_or_create(
-        name='Pro Users',
-        permissions=[FREE_PERMISSION, BASIC_PERMISSION, PRO_PERMISSION]
-    )
-    PREMIUM_GROUP, premium_created = Group.objects.get_or_create(
-        name='Premium Users',
-        permissions=[FREE_PERMISSION, BASIC_PERMISSION, PRO_PERMISSION, PREMIUM_PERMISSION]
-    )
+    FREE_GROUP, free_created = Group.objects.get_or_create(name='Free Users')
+    BASIC_GROUP, basic_created = Group.objects.get_or_create(name='Basic Users')
+    PRO_GROUP, pro_created = Group.objects.get_or_create(name='Pro Users')
+    PREMIUM_GROUP, premium_created = Group.objects.get_or_create(name='Premium Users')
 
-    groups = {
+    FREE_GROUP.permissions.set([permissions['fr']])
+    BASIC_GROUP.permissions.set([
+        permissions['fr'],
+        permissions['bc']
+    ])
+    PRO_GROUP.permissions.set([
+        permissions['fr'],
+        permissions['bc'],
+        permissions['pr']
+    ])
+    PREMIUM_GROUP.permissions.set([
+        permissions['fr'],
+        permissions['bc'],
+        permissions['pr'],
+        permissions['pm']
+    ])
+
+def get_groups():
+    from django.contrib.auth.models import Group
+    FREE_GROUP = Group.objects.get(name='Free Users')
+    BASIC_GROUP = Group.objects.get(name='Basic Users')
+    PRO_GROUP = Group.objects.get(name='Premium Users')
+    PREMIUM_GROUP = Group.objects.get(name='Pro Users')
+
+    return {
         'fr': FREE_GROUP,
         'bc': BASIC_GROUP,
         'pr': PRO_GROUP,
         'pm': PREMIUM_GROUP
     }
-except Exception as e:
-    print(e)
+
 
 def migrate_user(user):
+    from accounts.models import Profile
     current_plan = Profile.objects.get(user=user).plan
     user_group = user.groups.get()
     # print(f"Current plan: {current_plan}, current group: {user_group}")
     
+    groups = get_groups()
+
     for plan, group in groups.items():
         if current_plan == plan and user_group == group:
             pass
