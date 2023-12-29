2c8c3b7156348d200725acea7521d8d5229ba070
21stPhenom <phenom.h.acks21@gmail.com>
2023-12-29 14:41:36 +0100

    wrote logic for creating permissions after migrating

--- accounts/permissions.py
+++ accounts/permissions.py
@@ -1,9 +1,10 @@
-from django.contrib.auth.models import Permission, Group, User
-from django.contrib.contenttypes.models import ContentType
-from django.db.utils import OperationalError
-from accounts.models import Profile
+# Signal handler for creating permissions after migrating
+def create_permissions(sender, **kwargs):
+    from django.contrib.auth.models import Permission
+    from django.contrib.contenttypes.models import ContentType
 
-try:
+    from accounts.models import Profile
+
     profile_contenttype = ContentType.objects.get_for_model(Profile)
 
     FREE_PERMISSION, free_created = Permission.objects.get_or_create(
@@ -26,18 +27,26 @@ try:
         codename='can_view_premium',
         content_type=profile_contenttype
     )
-except Exception as e:
-    print(e)
 
-def check_permissions(user=None, tag=None):
-    assert user != None and tag != None, 'user and tag must be supplied'
-    user_permissions = user.groups.get().permissions.all()
-    permissions_dict = {
+def get_permissions():
+    from django.contrib.auth.models import Permission
+    FREE_PERMISSION = Permission.objects.get(codename='can_view_free')
+    BASIC_PERMISSION = Permission.objects.get(codename='can_view_basic')
+    PRO_PERMISSION = Permission.objects.get(codename='can_view_pro')
+    PREMIUM_PERMISSION = Permission.objects.get(codename='can_view_premium')
+
+    return {
         'fr': FREE_PERMISSION,
         'bc': BASIC_PERMISSION,
         'pr': PRO_PERMISSION,
         'pm': PREMIUM_PERMISSION,
     }
+
+def check_permissions(user=None, tag=None):
+    assert user != None and tag != None, 'user and tag must be supplied'
+    
+    user_permissions = user.groups.get().permissions.all()
+    permissions_dict = get_permissions()
 
     if tag in permissions_dict.keys() and permissions_dict[tag] in user_permissions:
         return True
